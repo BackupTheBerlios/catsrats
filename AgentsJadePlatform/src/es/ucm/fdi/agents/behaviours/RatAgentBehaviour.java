e56c242696910d3d8c346ba833a6b2414e87a09e
nachord <nachord>
2008-04-12 17:54:26 +0000

    Movimiento circular para los ratones

--- AgentsJadePlatform/src/es/ucm/fdi/agents/behaviours/RatAgentBehaviour.java
+++ AgentsJadePlatform/src/es/ucm/fdi/agents/behaviours/RatAgentBehaviour.java
@@ -17,25 +17,46 @@ public class RatAgentBehaviour extends TickerBehaviour{
 	private YellowPages paginasAmarillas;
 	private enum Orientacion{N,S,E,O,NE,NO,SE,SO};
 	private Orientacion orientacion;
+	private double trayectoria;
+	private double anguloTrayectoriaCircular;
 	
 	public RatAgentBehaviour(Agent agente, long period) {
 		super(agente, period);	
 		this.coordX= Math.random();
 		this.coordY= Math.random();
-		this.coordZ= Math.random()*5;
+		this.coordZ= 0.0;
 		this.nombre= myAgent.getLocalName();
 		this.orientacion = Orientacion.E;
 		this.activado = false;
 		paginasAmarillas= new YellowPages();
+		this.trayectoria = (Math.random()*10)%2;
+		this.anguloTrayectoriaCircular = 0.0;
 	}
 
 	protected String generaCoordenadas() {
-		coordX = coordX + Math.random()/10;
-		coordY = coordY + Math.random()/10;
-		coordZ = coordZ - Math.random()/10;
+		
+		//Movimiento en circulo
+		//if(trayectoria==0)
+			trayectoriaCircular();
+		//else
+			//trayectoriaZigZag();
+		
 		String mensaje= nombre+","+coordX+","+coordY+","+coordZ;
 		
 		return mensaje;	
+	}
+
+	private void trayectoriaZigZag() {
+		//TODO
+		
+	}
+
+	private void trayectoriaCircular() {
+		double radio = 3;
+		double incremento = 5*Math.PI/180; //incrementamos el angulo en 5 grados
+		coordX = radio*Math.cos(anguloTrayectoriaCircular);
+		coordY = radio*Math.sin(anguloTrayectoriaCircular);
+		anguloTrayectoriaCircular += incremento;
 	}
 
 	protected void onTick() {
